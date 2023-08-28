4667b114515cbd45e125ce307cfc67764ae0c660
sathwik reddy komandla <sathwikreddykomandla@gmail.com>
2023-08-28 12:20:12 +0530

    Rest Api using DRF

---
+++ Leadmanager/Leads/api.py
@@ -0,0 +1,13 @@
+from .serializers import LeadSerializer
+from .models import Lead
+from rest_framework import viewsets,permissions
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset=Lead.objects.all()
+    serializer_class=LeadSerializer
+    permission_classes=[
+        permissions.AllowAny
+    ]
+
+    
