3f21808b16a0665e9a11e7a03c0cb18777c3b853
sathwik reddy komandla <sathwikreddykomandla@gmail.com>
2023-08-28 12:37:36 +0530

    Added Shell Script to Create the Django Backend

---
+++ shellscript.bat
@@ -0,0 +1,16 @@
+::Creating the django Backend
+django-admin starproject Leadmanager
+
+::Naviagte to project directory
+cd Leadmanager
+
+::Creating a new app
+python manage.py startapp Leadmanager
+
+::Fetching all migrations
+python manage.py makemigrations
+
+
+::Applying all migrations
+python manage.py migrate
+
